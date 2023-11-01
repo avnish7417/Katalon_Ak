fad5e09ccf4aa0eab7474edc6cc4e1317eee27a2
avnish <avnishkumar@binmile.com>
2023-11-01 15:57:18 +0530

    visual testing suite

---
+++ Test Suites/Visual_Testing_Test/VisualTesting1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>VisualTesting1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>21c75e82-fbda-4400-80f3-0e26d0e96533</testSuiteGuid>
+   <testCaseLink>
+      <guid>1ffd01d9-b014-4c8d-8479-18ab6e56b764</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/visual Testing/New Test Case 1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
